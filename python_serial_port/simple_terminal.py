997c19a47c1b2b4862663786e16c4aa83d0be902
Panav <63401208+Pana1v@users.noreply.github.com>
2024-03-11 03:32:18 +0530

    Add Arduino communication code to simple_terminal.py

--- python_serial_port/simple_terminal.py
+++ python_serial_port/simple_terminal.py
@@ -1,17 +1,23 @@
-import serial
+# check this for a great simple input/output script:
+# http://shallowsky.com/software/scripts/ardmonitor
 
-serialPort = serial.Serial(port="COM1", baudrate=9600)
+import serial
+import time
+
+# reading data from Arduino
+arduino = serial.Serial("COM4", 9600)
 
 while True:
-        # Read only one byte from serial port
-        serialPortByte = serialPort.read(1)
+    arduino.readline()
+    time.sleep(2)
 
-        try:
-            # Convert byte to string (character)
-            char = serialPortByte.decode("ascii")
-        except UnicodeDecodeError:
-            # Don't raise error if byte is not ASCII
-            pass
-        else:
-            # Print the received byte in Python terminal
-            print(char, end="")
+# writing data to Arduino
+import serial # if you have not already done so
+arduino = serial.Serial('/dev/tty.usbserial', 9600)
+time.sleep(2)
+arduino.write('5')
+
+
+# Arduino resets on serial connect, add time.sleep(2) before writing
+# to disable this feature:
+# http://arduino.cc/playground/Main/DisablingAutoResetOnSerialConnection
